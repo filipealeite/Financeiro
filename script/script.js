e25c9af393770af950c563946bd085dabf8f0ad0
filipealeite <filipeaugustomachadoleite@gmail.com>
2025-02-15 18:07:11 -0300

    Realizado a personalização da navbar e iniciado a criação da funcionalidade de cadastro de contas

--- script/script.js
+++ script/script.js
@@ -2,13 +2,14 @@ function navBarOpen(){
     document.getElementById("sideBar").classList.remove("hidden");
     document.getElementById("navBar").classList.remove("hiddenNavBar");
     document.getElementById("content").classList.remove("hiddenContent");
+    
     // Buttons function
     setTimeout(() => {
         document.getElementById("buttonOpen").classList.add("buttonHide");
         document.getElementById("buttonOpen").classList.remove("buttonShow");
     },300);
 
-
+    document.getElementById("buttonOpen").style.display="none";    
 }
 function navBarClose(){
     document.getElementById("sideBar").classList.add("hidden");
@@ -16,8 +17,24 @@ function navBarClose(){
     document.getElementById("content").classList.add("hiddenContent");
     // Buttons function
     setTimeout(() => {
+        document.getElementById("buttonOpen").style.display="flex";    
         document.getElementById("buttonOpen").classList.remove("buttonHide");
         document.getElementById("buttonOpen").classList.add("buttonShow");
     },300);
     
 }
+const modal = document.querySelector("[data-model]");
+const openButton = document.querySelector("[data-open-modal]");
+const cleseButton = document.querySelector("[data-close-modal]");
+openButton.addEventListener("click",()=>{
+    modal.showModal();
+});
+
+
+function exibirCadastro(){
+
+    
+
+}
+
+
