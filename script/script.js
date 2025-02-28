bf382270a54fc9aa157cc51c9056aadd03a87455
filipealeite <filipeaugustomachadoleite@gmail.com>
2025-02-28 01:10:20 -0300

    Modificado a sidebar incluindo icones e alterando seu funcionamento, tambem foi criado uma tabela que sera utilizada para cadastro e exclusao de despesas, incluido opcao de exluir conta

--- script/script.js
+++ script/script.js
@@ -56,21 +56,30 @@ async function getItens(){
         if(list.length > 0){
             list.forEach(element => {
                 var div = document.createElement("div")
+                var divValorContainer = document.createElement("div")
                 document.getElementById("main-contas").appendChild(div)
+                
                 
                 var labelId = document.createElement("label")
                 var labelDescricao = document.createElement("label")
                 var labelValor = document.createElement("label")
-                
+                var labelIcone = document.createElement("span")
                 div.appendChild(labelId)
                 div.appendChild(labelDescricao)
-                div.appendChild(labelValor)
-
-                labelId.innerHTML = `Id: ${element.id}`
+                divValorContainer.appendChild(labelValor)
+                divValorContainer.appendChild(labelIcone)
+                div.appendChild(divValorContainer)
+                // div.appendChild(labelIcone)
+                divValorContainer.classList.add("divContainerValor")
+                labelId.innerHTML = `Conta: ${ String("000000" + element.id).slice(-6)}`
                 labelDescricao.innerHTML = `Descricao: ${element.descricao}`
-                labelValor.innerHTML = `Valor: ${element.valor}`
-                
+                labelValor.innerHTML = `Valor em Conta: ${element.valor}`
+                labelIcone.innerHTML = `
+                <svg class="h-6 w-6 text-slate-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="4" y1="7" x2="20" y2="7" />  <line x1="10" y1="11" x2="10" y2="17" />  <line x1="14" y1="11" x2="14" y2="17" />  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>`
                 div.classList.add("conta")
+                labelIcone.classList.add("trash")
+
+
             });
             console.log("work it")
         }
